2658646f063b66045588e3e8705d4faed63e0e81
ricksfelix <ricksfelix@gmail.com>
2018-07-24 20:07:50 -0300

    Banco de dados

--- DB/Database.py
+++ DB/Database.py
@@ -1,4 +1,15 @@
+from tkinter import *
+from tkinter import ttk
 import sqlite3
+import time
+
+
+#--------------------------------------
+#   DEFININDO MODULO HORA E DATA
+#--------------------------------------
+time = time.localtime()
+hour = ('{}:{}'.format(time[3], time[4]))
+date = ('{}/{}/{}'.format(time[0], time[1], time[2]))
 
 
 #--------------------------------------
@@ -6,8 +17,13 @@ import sqlite3
 #--------------------------------------
 con = sqlite3.connect('database.db')
 c = con.cursor()
-c.execute('CREATE TABLE IF NOT EXISTS Users(user text, passw text, cargo text)')
 sql = 'SELECT * FROM Users WHERE user = ?'
+clt = 'INSER INTO Clientes(data, funcionario, nome, cpf, tel, email) VALUES (?,?,?,?,?,?)'
+
+c.execute('CREATE TABLE IF NOT EXISTS Users(user text, passw text, cargo text)')
+c.execute("""
+CREATE TABLE IF NOT EXISTS Clientes(data text, funcionario text, nome text, cpf text, tel text,email text)
+""")
 
 
 #--------------------------------------
@@ -30,5 +46,52 @@ def login(user, passw):
 #--------------------------------------
 def cargo(user):
     c.execute(sql, (user,))
+    global auth
     auth = c.fetchone()
     return auth[2]
+
+
+#--------------------------------------
+#       CADASTRO DE CLIENTES
+#--------------------------------------
+def cadastro():
+
+
+#--------------------------------------
+#       GESTOR DE INFORMAÇÃO
+#--------------------------------------
+    def get():
+        data = "{} {}".format(date, hour)
+        user = auth[0]
+        cargo = auth[2]
+        name = et_name.get()
+        cpf = et_cpf.get()
+        tel = et_tel.get()
+        email = et_email.get()
+        c.execute('')
+
+    root = Tk()
+    cad = LabelFrame(root, text='Cadastro')
+    root.title("S4U® CADASTRO")
+    Label(cad, text='Nome').grid(row=0, column=0)
+    Label(cad, text='CPF').grid(row=1, column=0)
+    Label(cad, text='Telefone').grid(row=2, column=0)
+    Label(cad, text='E-Mail').grid(row=3, column=0)
+
+    et_name = Entry(cad)
+    et_cpf = Entry(cad)
+    et_tel = Entry(cad)
+    et_email = Entry(cad)
+
+    et_name.grid(row=0, column=1)
+    et_cpf.grid(row=1, column=1)
+    et_tel.grid(row=2, column=1)
+    et_email.grid(row=3, column=1)
+    cad.grid(row=0, columnspan=4)
+
+    Button(root, text='Salvar', command=get).grid(row=1, column=0, sticky=W+E)
+    Button(root, text='Cadastrar Equipamento').grid(row=1, column=1, sticky=W+E)
+    Button(root, text='Limpar').grid(row=1, column=2, sticky=W+E)
+    Button(root, text='Sair').grid(row=1, column=3, sticky=W+E)
+
+    root.mainloop()
--- Def/Def.py
+++ Def/Def.py
@@ -1,7 +1,10 @@
 from DB import Database
 from tkinter import *
 from tkinter import ttk
-
+import time
+time = time.localtime()
+hour = ('{}:{}'.format(time[3], time[4]))
+date = ('{}/{}/{}'.format(time[0], time[1], time[2]))
 
 #-------------------------------------------
 #  CONSULTANDO AO GESTOR DE BANCO DE DADOS
@@ -19,15 +22,19 @@ def login(user, passw):
 def menu(user):
     cargo = Database.cargo(user)
     root = Tk()
+    root.geometry("1024x768+0+0")
     root.title('S4U® Bem Vindo {}'.format(user))
+    title = Label(root, text='Seja Bem Vindo "{} {}"\n{}\n{}'.format(cargo.title(), user.title(), date, hour))
+    title.grid(row=0, columnspan=2, sticky=W+E)
 
 
 #--------------------------------------
 #      INTERFACE DE CADASTRO
 #--------------------------------------
     cad = LabelFrame(root, text='Cadastro')
-    cad.grid(row=0,columnspan=2, sticky=W+E)
-    Button(cad, text='CADASTRAR').grid(row=0, column=0, sticky=W+E)
+    cad.grid(row=1,columnspan=2, sticky=W+E)
+    Button(cad, text='CADASTRAR', command=Database.cadastro).grid(row=0, column=0, sticky=W+E)
+
 
 #--------------------------------------
 #      INTERFACE DE CONFIGURAÇÃO
@@ -35,18 +42,20 @@ def menu(user):
     conf = LabelFrame(root, text='Configurações')
     Button(conf, text='CONFIGURAR').grid(row=0, column=0, sticky=W+E)
 
+
 #--------------------------------------
 #      INTERFACE DE DADOS
 #--------------------------------------
     dados = LabelFrame(root, text='Dados')
     Button(dados, text='DADOS').grid(row=0, column=0, sticky=W+E)
 
+
 #--------------------------------------
 #      GESTOR DE PERMISSÃO
 #--------------------------------------
     if cargo == 'admin' or cargo == 'fiscal':
-        dados.grid(row=2,columnspan=2, sticky=W+E)
+        dados.grid(row=3,columnspan=2, sticky=W+E)
     if cargo == 'admin':
-        conf.grid(row=1,columnspan=2, sticky=W+E)
+        conf.grid(row=2,columnspan=2, sticky=W+E)
 
     root.mainloop()
